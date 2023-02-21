b2c7050f7bd70e339b8deb8a58f163a9b5fb0581
harryjmg <harry.gueguen@gmail.com>
2023-02-21 15:41:39 +0100

    Now everything is JS

--- esclave.js
+++ esclave.js
@@ -17,9 +17,26 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function envoyer_sms(contenu_sms) {
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+        .create({
+            body: contenu_sms,
+            from: process.env.TWILIO_FROM_PHONE,
+            to: process.env.TWILIO_TO_PHONE
+        })
+        .then(message => console.log("Sms envoyé ! SID: " + message.sid));
+}
+
 async function run () {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
+
+    // 0. Donner date et heure de la vérification
+    await console.log("Vérification le " + new Date().toLocaleString());
 
     // 1. Se connecter
     await page.goto(url_de_connexion);
@@ -37,16 +54,25 @@ async function run () {
     await sleep(2000);
 
     // 4. Vérifier les dates
+    let dates_libres = [];
     for (let date_a_verifier of dates_a_verifier) {
         await page.waitForSelector(date_a_verifier);
-        if (await page.$eval(date_a_verifier, el => el.disabled)) {
-            await console.log(date_a_verifier + ' Rien de dispo !');
-        } else {
-            await console.log(date_a_verifier + ' LIBRE !');
+        if (await page.$eval(date_a_verifier, el => el.disabled === false)) {
+            await dates_libres.push(date_a_verifier);
         }
+    }
+
+    // 5. Envoyer un sms si des dates sont libres
+    if (dates_libres.length > 0) {
+        await console.log("\x1b[32m%s\x1b[0m", "Dates libres : " + dates_libres);
+        await envoyer_sms("Dates libres : " + dates_libres);
+        await process.exit(0);
+    } else {
+        await console.log("\x1b[31m%s\x1b[0m", "Aucune date libre");
     }
 
     await browser.close();
 }
 
 run();
+setInterval(run, 600000);
